e80b921ee72e3043ac982fa092008661924df616
Spyros <spgrvl@gmail.com>
2020-11-11 18:32:52 +0200

    Added domain checker. Names are now suggested only if .com domain is available.

---
+++ domain_checker.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import re
+import time
+
+if os.name == "nt":
+	chrome_path = r"C:\chromedriver_win32\chromedriver.exe"
+elif os.name == "posix":
+	chrome_path = "/usr/lib/chromium-browser/chromedriver"
+else:
+	print("Error: Chrome path is not set for this os: " + os.name)
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
+
+def check_availability(name):
+    url = "https://instantdomainsearch.com/#search={}.com".format(name)
+    driver.get(url)
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="gatsby-focus-wrapper"]/main/section[2]/div[1]/div/div[1]/div/a'))
+    )
+    href = element.get_attribute('href')
+    href_domain = re.search(r'^(?:http(?:s?)://)(?:www.)?([\w]+)\.[a-zA-Z]+', href)[1]
+    driver.get("about:blank")
+    if href_domain == name:
+        return False
+    else:
+        return True
+
+def quit_webdriver():
+    driver.quit()
--- generator.py
+++ generator.py
@@ -1,6 +1,18 @@
 from lists.nouns import get_nouns
 from lists.birds import get_bird_names
+from domain_checker import check_availability, quit_webdriver
 import random
+
+def find_name(name_type):
+    if name_type == "noun":
+        names = get_nouns()
+    elif name_type == "bird":
+        names = get_bird_names()
+    domain_available = False
+    while domain_available == False:
+        startup_name = names[random.randint(0,len(names)-1)]
+        domain_available = check_availability(startup_name.lower())
+    print("\nYour startup name is {} and domain {}.com is available!".format(startup_name.capitalize(), startup_name.lower()))
 
 choice=True
 while choice:
@@ -17,14 +29,11 @@ while choice:
     """)
     choice=input("What would you like to do? ")
     if choice=="1":
-        nouns = get_nouns()
-        startup_name = nouns[random.randint(0,len(nouns)-1)].capitalize()
-        print("\nYour startup name is", startup_name)
+        find_name('noun')
     elif choice=="2":
-        birds = get_bird_names()
-        startup_name = birds[random.randint(0,len(birds)-1)].capitalize()
-        print("Your startup name is", startup_name)
+        find_name('bird')
     elif choice=="0":
         choice = False
     else:
         print("\nNot a valid choice, try again!")
+quit_webdriver()
